8e6eefac606ef3a8b55e6300c529a511965a4bc8
hugochan <hugochan2013@gmail.com>
2015-03-02 17:55:13 +0800

    update binary search tree

--- searching/binary_search_tree.py
+++ searching/binary_search_tree.py
@@ -12,22 +12,7 @@ class Node(object):
 class BinarySearchTree(object):
     """docstring for BinarySearchTree"""
     def __init__(self):
-        self.__root = None
-        
-    def isEmpty(self):
-        return self.size() is 0
-
-    def size(self):
-        return self.__size(self.__root)
-
-    def __size(self, x):
-        if x is None:
-            return 0
-        else:
-            return x.N
-
-    def contains(self, key):
-        return self.get(key) is not None
+        self.__root = None    
 
     def get(self, key):
         return self.__get(self.__root, key)
@@ -59,6 +44,49 @@ class BinarySearchTree(object):
         x.N = self.__size(x.left) + self.__size(x.right) + 1
         return x
 
+    def delete(self, key):
+        self.__root = self.__delete(self.__root, key)
+
+    def __delete(self, x, key):
+        if x is None:
+            return None
+        _cmp = self.__compare(key, x.key)
+        if _cmp < 0:
+            x.left = self.__delete(x.left, key)
+        elif _cmp > 0:
+            x.right = self.__delete(x.right, key)
+        else:
+            if x.right is None:
+                return x.left
+            elif x.left is None:
+                return x.right
+            t = x
+            x = self.__min(t.right)
+            x.right = self.__deleteMin(t.right)
+            x.left = t.left
+        x.N = self.__size(x.left) + self.__size(x.right) + 1
+        return x
+
+    def deleteMin(self):
+        self.__root = self.__deleteMin(self.__root)
+
+    def __deleteMin(self, x):
+        if x.left is None:
+            return x.right
+        x.left = self.__deleteMin(x.left)
+        x.N = self.__size(x.left) + self.__size(x.right) + 1
+        return x
+
+    def deleteMax(self):
+        self.__root = self.__deleteMax(self.__root)
+
+    def __deleteMax(self, x):
+        if x.right is None:
+            return x.left
+        x.right = self.__deleteMax(x.right)
+        x.N = self.__size(x.left) + self.__size(x.right) + 1
+        return x
+
     def max(self):
         return self.__max(self.__root).key
 
@@ -76,6 +104,26 @@ class BinarySearchTree(object):
             return x
         else:
             return self.__min(x.left)
+
+    def keys(self, lo=None, hi=None):
+        if lo is None and hi is None:
+            return self.keys(self.min(), self.max())
+        else:
+            queue = []
+            self.__keys(self.__root, queue, lo, hi)
+            return queue
+
+    def __keys(self, x, queue, lo, hi):
+        if x is None:
+            return None
+        cmplo = self.__compare(lo, x.key)
+        cmphi = self.__compare(hi, x.key)
+        if cmplo < 0:
+            self.__keys(x.left, queue, lo, hi)
+        if cmplo <= 0 <= cmphi:
+            queue.append(x.key)
+        if cmphi > 0:
+            self.__keys(x.right, queue, lo, hi)
 
     def floor(self, key):
         x = self.__floor(self.__root, key)
@@ -147,68 +195,48 @@ class BinarySearchTree(object):
         else:
             return self.__size(x.left)
 
-    def delete(self, key):
-        self.__root = self.__delete(self.__root, key)
+    def contains(self, key):
+        return self.get(key) is not None
 
-    def __delete(self, x, key):
+    def isEmpty(self):
+        return self.size() is 0
+
+    def size(self):
+        return self.__size(self.__root)
+
+    def __size(self, x):
         if x is None:
-            return None
-        _cmp = self.__compare(key, x.key)
-        if _cmp < 0:
-            x.left = self.__delete(x.left, key)
-        elif _cmp > 0:
-            x.right = self.__delete(x.right, key)
+            return 0
         else:
-            if x.right is None:
-                return x.left
-            elif x.left is None:
-                return x.right
-            t = x
-            x = self.__min(t.right)
-            x.right = self.__deleteMin(t.right)
-            x.left = t.left
-        x.N = self.__size(x.left) + self.__size(x.right) + 1
-        return x
+            return x.N
 
-    def deleteMin(self):
-        self.__root = self.__deleteMin(self.__root)
+    def height(self):
+        """height of tree (1-node tree has height 0)"""
+        return self.__height(self.__root)
 
-    def __deleteMin(self, x):
-        if x.left is None:
-            return x.right
-        x.left = self.__deleteMin(x.left)
-        x.N = self.__size(x.left) + self.__size(x.right) + 1
-        return x
+    def __height(self, x):
+        if x is None: 
+            return -1;
+        return 1 + max(self.__height(x.left), self.__height(x.right))
 
-    def deleteMax(self):
-        self.__root = self.__deleteMax(self.__root)
+    def isBST(self):
+        """does this binary tree satisfy symmetric order?
+        Note: this test also ensures that data structure is a binary tree since order is strict
+        """
+        return self.__isBST(self.__root, None, None);
 
-    def __deleteMax(self, x):
-        if x.right is None:
-            return x.left
-        x.right = self.__deleteMax(x.right)
-        x.N = self.__size(x.left) + self.__size(x.right) + 1
-        return x
-
-    def keys(self, lo=None, hi=None):
-        if lo is None and hi is None:
-            return self.keys(self.min(), self.max())
-        else:
-            queue = []
-            self.__keys(self.__root, queue, lo, hi)
-            return queue
-
-    def __keys(self, x, queue, lo, hi):
+    def __isBST(self, x, _min, _max):
+        """is the tree rooted at x a BST with all keys strictly between _min and _max
+        (if _min or _max is None, treat as empty constraint)
+        Credit: Bob Dondero's elegant solution
+        """
         if x is None:
-            return None
-        cmplo = self.__compare(lo, x.key)
-        cmphi = self.__compare(hi, x.key)
-        if cmplo < 0:
-            self.__keys(x.left, queue, lo, hi)
-        if cmplo <= 0 <= cmphi:
-            queue.append(x.key)
-        if cmphi > 0:
-            self.__keys(x.right, queue, lo, hi)
+            return True
+        if _min is not None and self.__compare(x.key, _min) <= 0:
+            return False
+        if _max is not None and self.__compare(x.key, _max) >= 0:
+            return False
+        return self.__isBST(x.left, _min, x.key) and self.__isBST(x.right, x.key, _max)
 
     def show(self):
         """print the bst"""
@@ -253,3 +281,5 @@ if __name__ == '__main__':
     print bst.get(0)
     print bst.contains(0)
     print bst.isEmpty()
+    print bst.height()
+    print bst.isBST()
