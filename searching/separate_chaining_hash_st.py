5c463cc2f8c0bc533858b15f32657d17320768f9
hugochan <hugochan2013@gmail.com>
2015-03-11 13:25:49 +0800

    update separate chaining hash st: resize, put...

--- searching/separate_chaining_hash_st.py
+++ searching/separate_chaining_hash_st.py
@@ -5,22 +5,49 @@ from sequential_search_st import SequentialSearchST as ssst
 class SeparateChainingHashST(object):
     """docstring for SeparateChainingHashST"""
     def __init__(self, M=997):
-        self.__M = M
-        self.st = [0]*self.__M
-        for i in xrange(self.__M):
+        self.__N = 0
+        self.M = M
+        self.st = [0]*self.M
+        for i in xrange(self.M):
             self.st[i] = ssst()
 
     def get(self, key):
         return self.st[self.__hash(key)].get(key)
 
     def put(self, key, val):
-        self.st[self.__hash(key)].put(key, val)
+        if self.__N >= 2*self.M:
+            self.__resize(2*self.M)
+        if self.st[self.__hash(key)].put(key, val):
+            self.__N += 1
 
     def delete(self, key):
-        self.st[self.__hash(key)].delete(key)
+        if self.st[self.__hash(key)].delete(key):
+            self.__N -= 1
+        if self.__N > 0 and self.__N <= self.M/8:
+            self.__resize(self.M/2)
+
+    def size(self):
+        return self.__N
+
+    def show(self):
+        for i in xrange(0, self.M):
+            x = self.st[i].first
+            while x is not None:
+                print x.key, x.val
+                x = x.next
 
     def __hash(self, key):
-        return (hash(key) & 0x7fffffff) % self.__M
+        return (hash(key) & 0x7fffffff) % self.M
+
+    def __resize(self, cap):
+        t = SeparateChainingHashST(cap)
+        for i in xrange(0, self.M):
+            x = self.st[i].first
+            while x is not None:
+                t.put(x.key, x.val)
+                x = x.next
+        self.st = t.st
+        self.M = t.M
 
 if __name__ == '__main__':
     import random
@@ -29,10 +56,12 @@ if __name__ == '__main__':
     random.shuffle(keys)
     random.shuffle(vals)
     print keys, vals
-    st = SeparateChainingHashST(7)
+    st = SeparateChainingHashST()
     for i in xrange(10):
         st.put(keys[i], vals[i])
+    st.show()
     for i in xrange(9, -1, -1):
-        print keys[i], st.get(keys[i])
+        print st.get(keys[i])
     st.delete(keys[0])
     print st.get(keys[0])
+    print st.size()
