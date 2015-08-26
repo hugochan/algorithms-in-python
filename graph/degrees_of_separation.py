0ef419511847d771675f6bc8b582fecb9e6058cd
hugochan <hugochan2013@gmail.com>
2015-08-26 10:55:07 -0400

    add degrees of separation

---
+++ graph/degrees_of_separation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from symbol_graph import SymbolGraph
+from bfp import BreadthFirstPath
+
+def DegreesOfSeparation(file_in, sp, source):
+    sg = SymbolGraph(file_in, sp)
+    g = sg.G()
+    if not sg.contains(source):
+        print '%s not in database'%source
+        exit()
+
+    s = sg.index(source)
+    bfp = BreadthFirstPath(g, s)
+
+    dest = raw_input('Enter a target: ')
+    if not sg.contains(dest):
+        print 'Not in database'
+        exit()
+    t = sg.index(dest)
+    if bfp.has_path_to(t):
+        print '  %s'%source
+        for v in bfp.path_to(t)[::-1]:
+            print '  %s'%sg.name(v)
+    else:
+        print 'Not connected'
+
+if __name__ == '__main__':
+    DegreesOfSeparation('symbol_graph.txt', ' ', 'JFK')
